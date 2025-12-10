e026bba8b788b81b87f231bb979e11d1db8870e5
keliumJU <keliumju@gmail.com>
2025-12-09 22:55:32 -0500

    Add manifesto text

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
-import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1>Kelium J.U.</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <h1 className="text-3xl font-bold underline">
+        <span className="bg-blue-500">Kelium J.U</span> Manifesto
+      </h1>
+      <div className="rounded-2xl bg-amber-500 font-bold text-white p-3">
+        <p className="text-5xl text-pretty mb-5">
+          For along time i was running away of competitive programming(CP), i
+          think that this is a very important thing that i need to do, and
+          practice conscientiously, i need to do this for my carrer. Every day
+          for all day or almost all days i want to practice for 1 hour minimun
+          CP or leetcode, i want to begin with neetcode guides and after move
+          with CP, i want to love solve problems or try to solve problems, and
+          maybe i can improve some my english level(prayer).
+        </p>
+        <div className="flex flex-col gap-y-3">
+          <span className="italic text-4xl bg-amber-700">
+            That is all, folks, bye fish
+          </span>
+          <span className="italic text-4xl bg-green-500">
+            Note: I want to put all my days resume here(problems related) so
+            tomorrow i can setup a markdown blog
+          </span>
+        </div>
       </div>
     </>
   );
