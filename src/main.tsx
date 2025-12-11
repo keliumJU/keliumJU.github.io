2c2ea099998f3b2589cddd8871cbae3370467aac
keliumJU <keliumju@gmail.com>
2025-12-10 23:05:41 -0500

    Add any type to buffer

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { Buffer } from "buffer";
 import "./index.css";
 import App from "./App.tsx";
 
-globalThis.Buffer = Buffer;
+(globalThis as any).Buffer = Buffer;
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
